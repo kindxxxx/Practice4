64d41b5f79ca5b3a9ceed2dab3b3e04b63c6920d
kindxxxx <sava.molchanov.07@gmail.com>
2026-02-19 09:39:30 +0500

    git commit -m "Add custom iterator example"

--- generators.py
+++ generators.py
@@ -10,3 +10,19 @@ it_word = iter(word)
 
 for letter in it_word:
     print(letter)
+
+class SimpleCounter:
+    def __init__(self, limit):
+        self.limit = limit
+        self.current = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current > self.limit:
+            raise StopIteration
+
+        value = self.current
+        self.current += 1
+        return value
