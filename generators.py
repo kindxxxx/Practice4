0ba79b057c79e95391222ebb587a8a162587f2dd
kindxxxx <sava.molchanov.07@gmail.com>
2026-02-19 09:47:09 +0500

    for value in simple_generator():

--- generators.py
+++ generators.py
@@ -41,6 +41,16 @@ def simple_generator():
     yield 3
 
 
+
+
 for value in simple_generator():
     print(value)
 
+def count_down(n):
+    while n > 0:
+        yield n
+        n -= 1
+
+
+for number in count_down(3):
+    print(number)
